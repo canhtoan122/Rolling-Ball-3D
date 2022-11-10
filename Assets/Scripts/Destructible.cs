3cdb8e086f905bbac39b93cd345014abaacf96f8
canhtoan122 <74394729+canhtoan122@users.noreply.github.com>
2022-11-10 16:09:10 +0700

    Initial Commit

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Destructible : MonoBehaviour {
+
+	public Transform player;
+	public Transform pieces;
+
+	public Text gameoverText;
+	public GameObject gameoverPanel;
+	public GameManager gameManager;
+
+	public void Destroy () {
+
+		gameObject.GetComponent<Rigidbody>().isKinematic = true;
+		gameObject.GetComponent<Collider>().enabled = false;
+		//player.gameObject.SetActive (false);
+		//pieces.gameObject.SetActive (true);
+		gameoverText.text = "GAME OVER";
+		gameManager.gameOver.Play();
+		GameManager.endGame = true;
+		Time.timeScale = 0f;
+
+		gameoverPanel.SetActive(true);
+	}
+
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# rollingball3D
+Simple Unity3D game (based on the roll a ball example with a lot of tweaks and additions). You have to control a ball to collect all the pickups and kill the enemies in few different levels
+
+# Demo video
+<a target="_blank" href="http://www.youtube.com/watch?v=nB-meZX1VGA"><img src="http://img.youtube.com/vi/nB-meZX1VGA/0.jpg" alt="OpenVR scene" width="240" height="180" border="10" /></a>
